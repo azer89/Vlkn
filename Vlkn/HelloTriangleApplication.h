315f702de13d6d5868a1c79374f7fe73c40ef312
Reza Saputra <radhitya@uwaterloo.ca>
2022-01-26 17:20:01 -0500

    Update HelloTriangleApplication.h

--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -4,8 +4,25 @@
 class HelloTriangleApplication
 {
 public:
+    void run() 
+    {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
 private:
+    void initVulkan() 
+    {
+    }
+
+    void mainLoop() 
+    {
+    }
+
+    void cleanup() 
+    {
+    }
 
 };
 
