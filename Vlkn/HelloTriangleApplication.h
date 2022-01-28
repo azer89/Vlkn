0d7de9a07532b356ac3dccab597f76daedf9df05
Reza Saputra <radhitya@uwaterloo.ca>
2022-01-27 20:38:57 -0500

    Update HelloTriangleApplication.h

--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -1,6 +1,16 @@
 #ifndef __HELLO_TRIANGLE_APPLICATION_H__
 #define __HELLO_TRIANGLE_APPLICATION_H__
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+//const uint32_t WIDTH = 800;
+//const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication
 {
 public:
