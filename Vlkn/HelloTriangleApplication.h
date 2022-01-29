0b4fe8d8a5cf7fd4a6f5a345feda9641310e10a7
Reza Saputra <radhitya@uwaterloo.ca>
2022-01-29 17:57:02 -0500

    Update HelloTriangleApplication.h

--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -8,9 +8,6 @@
 #include <stdexcept>
 #include <cstdlib>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-
 class HelloTriangleApplication
 {
 public:
@@ -24,16 +21,36 @@ public:
 private:
     GLFWwindow* window;
 
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    void initWindow() 
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() 
     {
     }
 
     void mainLoop() 
     {
+        while (!glfwWindowShouldClose(window)) 
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() 
     {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
     }
 
 };
