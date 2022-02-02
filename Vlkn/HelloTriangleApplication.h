4da75a8a4e3196d721bcccc074576f5e2a6980f8
Reza Saputra <radhitya@uwaterloo.ca>
2022-02-02 17:52:36 -0500

    Update HelloTriangleApplication.h

--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -4,6 +4,18 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication
 {
 private:
