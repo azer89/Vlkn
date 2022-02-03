83246d7bb2a84cfbd25dabbb5be1603939a8f7c7
Reza Saputra <radhitya@uwaterloo.ca>
2022-02-03 18:57:58 -0500

    validation layer

--- Vlkn/HelloTriangleApplication.cpp
+++ Vlkn/HelloTriangleApplication.cpp
@@ -4,6 +4,17 @@
 
 #include "HelloTriangleApplication.h"
 
+const std::vector<const char*> validationLayers = 
+{
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 void HelloTriangleApplication::run()
 {
     initWindow();
--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -6,16 +6,6 @@
 
 #include <vector>
 
-const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
-};
-
-#ifdef NDEBUG
-const bool enableValidationLayers = false;
-#else
-const bool enableValidationLayers = true;
-#endif
-
 class HelloTriangleApplication
 {
 private:
