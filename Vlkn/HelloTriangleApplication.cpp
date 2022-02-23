4d7de5524c61d3895593a816a33a4aed08ebd0c1
Reza Saputra <radhitya@uwaterloo.ca>
2022-02-23 17:22:58 -0500

    Update HelloTriangleApplication.cpp

--- Vlkn/HelloTriangleApplication.cpp
+++ Vlkn/HelloTriangleApplication.cpp
@@ -119,6 +119,41 @@ void HelloTriangleApplication::pickPhysicalDevice()
     }
 }
 
+bool isDeviceSuitable(VkPhysicalDevice device) 
+{
+    QueueFamilyIndices indices = findQueueFamilies(device);
+    return indices.isComplete();
+}
+
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) 
+{
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) 
+    {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) 
+        {
+            indices.graphicsFamily = i;
+        }
+
+        if (indices.isComplete()) 
+        {
+            break;
+        }
+
+        i++;
+    }
+
+    return indices;
+}
+
 void HelloTriangleApplication::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) 
 {
     createInfo = {};
