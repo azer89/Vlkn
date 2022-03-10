60ca23d9f06c91198d4e3a8001d654cc526a8482
Reza Saputra <radhitya@uwaterloo.ca>
2022-03-10 16:49:13 -0500

    validation layer refactor

--- Vlkn/HelloTriangleApplication.cpp
+++ Vlkn/HelloTriangleApplication.cpp
@@ -2,9 +2,22 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <set>
+#include <algorithm>
+#include <cstdint>
+#include <limits>
 
 #include "HelloTriangleApplication.h"
-#include "ValidationLayer.h"
+
+const std::vector<const char*> validationLayers =
+{
+    "VK_LAYER_KHRONOS_validation"
+};
+
+//#ifdef NDEBUG
+//const bool enableValidationLayers = false;
+//#else
+const bool enableValidationLayers = true;
+//#endif
 
 void HelloTriangleApplication::run()
 {
@@ -312,6 +325,33 @@ bool HelloTriangleApplication::checkValidationLayerSupport()
     }
 
     return true;
+}
+
+VkResult HelloTriangleApplication::CreateDebugUtilsMessengerEXT(VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger)
+{
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr)
+    {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void HelloTriangleApplication::DestroyDebugUtilsMessengerEXT(VkInstance instance,
+    VkDebugUtilsMessengerEXT debugMessenger,
+    const VkAllocationCallbacks* pAllocator)
+{
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr)
+    {
+        func(instance, debugMessenger, pAllocator);
+    }
 }
 
 VKAPI_ATTR VkBool32 VKAPI_CALL HelloTriangleApplication::debugCallback(
--- Vlkn/HelloTriangleApplication.h
+++ Vlkn/HelloTriangleApplication.h
@@ -50,6 +50,13 @@ private:
     void setupDebugMessenger();
     std::vector<const char*> getRequiredExtensions();
     bool checkValidationLayerSupport();
+    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+        const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+        const VkAllocationCallbacks* pAllocator,
+        VkDebugUtilsMessengerEXT* pDebugMessenger);
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+        VkDebugUtilsMessengerEXT debugMessenger,
+        const VkAllocationCallbacks* pAllocator);
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
         VkDebugUtilsMessageTypeFlagsEXT messageType, 
         const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
