6c35d3aa2425578d618dc2cf18faabb111c45ec7
Reza Saputra <radhitya@uwaterloo.ca>
2022-03-12 17:46:40 -0500

    swap chain

---
+++ Vlkn/SwapChainSupportDetails.h
@@ -0,0 +1,16 @@
+#ifndef __SWAP_CHAIN_SUPPORT_DETAILS_H__
+#define __SWAP_CHAIN_SUPPORT_DETAILS_H__
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vector>
+
+struct SwapChainSupportDetails 
+{
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+#endif
