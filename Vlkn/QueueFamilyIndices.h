324c689827650957ebfda42f55815aeb96812fef
Reza Saputra <radhitya@uwaterloo.ca>
2022-04-06 19:11:22 -0400

    Update QueueFamilyIndices.h

--- Vlkn/QueueFamilyIndices.h
+++ Vlkn/QueueFamilyIndices.h
@@ -6,8 +6,8 @@
 struct QueueFamilyIndices 
 {
     // std::optional requires C++17
-    std::optional<uint32_t> graphicsFamily;
-    std::optional<uint32_t> presentFamily;
+    std::optional<uint32_t> graphicsFamily; // support drawing command?
+    std::optional<uint32_t> presentFamily; // support image presentation?
 
     bool isComplete() 
     {
